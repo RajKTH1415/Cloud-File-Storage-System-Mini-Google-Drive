03c221fdba8c98af80583541e620b45078aa6ef5
Rajkumar prasad <rajkumarprasadkth@gmail.com>
2026-06-18 17:14:00 +0530

    first commit

---
+++ cloud-storage-system/src/test/java/com/cloudFileStorageSystem/CloudStorageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudFileStorageSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
